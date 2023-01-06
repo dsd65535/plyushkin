88f6a4e0a1e0ec58cc0897d14c61ca9209e1df53
Dimitar Dimitrov <mitkodimitar@gmail.com>
2023-01-05 23:46:42 -0500

    Provide variables in instance name

--- plyushkin/opt/plyushkin/create_snapshot.sh
+++ plyushkin/opt/plyushkin/create_snapshot.sh
@@ -2,6 +2,19 @@
 
 set -xe
 
+if [[ -z "${PL_SETUP}" ]]; then
+    echo "PL_SETUP not defined"
+    exit 1
+fi
+
+TARGET=${PL_SETUP%:*}
+FS_NAME=${PL_SETUP#*:}
+
+if [[ -z "${TARGET}" ]]; then
+    echo "TARGET not defined"
+    exit 1
+fi
+
 if [[ -z "${FS_NAME}" ]]; then
     echo "FS_NAME not defined"
     exit 1
--- plyushkin/opt/plyushkin/send_snapshot.sh
+++ plyushkin/opt/plyushkin/send_snapshot.sh
@@ -2,13 +2,21 @@
 
 set -xe
 
-if [[ -z "${FS_NAME}" ]]; then
-    echo "FS_NAME not defined"
+if [[ -z "${PL_SETUP}" ]]; then
+    echo "PL_SETUP not defined"
     exit 1
 fi
 
+TARGET=${PL_SETUP%:*}
+FS_NAME=${PL_SETUP#*:}
+
 if [[ -z "${TARGET}" ]]; then
     echo "TARGET not defined"
+    exit 1
+fi
+
+if [[ -z "${FS_NAME}" ]]; then
+    echo "FS_NAME not defined"
     exit 1
 fi
 
